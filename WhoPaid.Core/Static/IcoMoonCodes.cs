83b3019706db0dcba40e4d94658a05cf254dce61
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2019-08-29 07:01:00 +0200

    #11 Add Font into Android & iOS Assets

---
+++ WhoPaid.Core/Static/IcoMoonCodes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WhoPaid.Core.Static
+{
+    public static class IcoMoonCodes
+    {
+        public const string Money = "\ue900";
+        public const string Left = "\uf053";
+        public const string Right = "\uf054";
+        public const string Plus = "\uf055";
+        public const string Check = "\uf05d";
+        public const string Ban = "\uf05e";
+        public const string Globe = "\uf0ac";
+        public const string Chart = "\uf200";
+    }
+}
