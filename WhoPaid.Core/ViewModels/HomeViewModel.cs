7c392b54d4f6006e0920913b1199f61d74a189fe
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2019-09-05 15:28:41 +0200

    #6 Well Designed Cell & Removing all local User Files

--- WhoPaid.Core/ViewModels/HomeViewModel.cs
+++ WhoPaid.Core/ViewModels/HomeViewModel.cs
@@ -1,10 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Reactive;
+using System.Threading.Tasks;
 using MvvmCross.ViewModels;
+using WhoPaid.Core.Models;
 using WhoPaid.Core.Services;
 using WhoPaid.ReactiveUI.Interop;
+using ReactiveUI;
 
 namespace WhoPaid.Core.ViewModels
 {
     public class HomeViewModel : MvxReactiveViewModel
     {
+        private readonly ObservableAsPropertyHelper<List<TaxPayer>> _taxPayers;
+        public List<TaxPayer> TaxPayers => _taxPayers.Value;
+
+        public ReactiveCommand<Unit, List<TaxPayer>> LoadTaxPayersCommand { get; }
+
+        public HomeViewModel()
+        {
+            LoadTaxPayersCommand = ReactiveCommand.Create<Unit, List<TaxPayer>>(_ => LoadTaxPayers());
+            //LoadTaxPayersCommand.ThrownExceptions.Subscribe(DisplayError);
+
+            _taxPayers = LoadTaxPayersCommand.ToProperty(this, nameof(TaxPayers));
+        }
+
+        public override Task Initialize()
+        {
+            LoadTaxPayersCommand.Execute().Subscribe();
+            return base.Initialize();
+        }
+
+        private List<TaxPayer> LoadTaxPayers()
+        {
+            return new List<TaxPayer>
+            {
+                new TaxPayer
+                {
+                    FullName = "Bohdan Benetskyi",
+                    MonthRate = 120,
+                    PaymentHistory = new List<Payment>
+                    {
+                        new Payment{IsPaid = true},
+                        new Payment{IsPaid = true},
+                        new Payment{IsPaid = false},
+                        new Payment{IsPaid = false},
+                    }
+                },
+                new TaxPayer
+                {
+                    FullName = "Iia Benetskya",
+                    MonthRate = 500,
+                    PaymentHistory = new List<Payment>
+                    {
+                        new Payment{IsPaid = true},
+                        new Payment{IsPaid = true},
+                        new Payment{IsPaid = true},
+                    }
+                },
+                new TaxPayer
+                {
+                    FullName = "Ruslan Benetskyi",
+                    MonthRate = 0,
+                    PaymentHistory = new List<Payment>
+                    {
+                        new Payment{IsPaid = true},
+                        new Payment{IsPaid = true},
+                        new Payment{IsPaid = true},
+                        new Payment{IsPaid = true},
+                        new Payment{IsPaid = false},
+                        new Payment{IsPaid = false},
+                        new Payment{IsPaid = false},
+                        new Payment{IsPaid = false},
+                        new Payment{IsPaid = false},
+                    }
+                }
+            };
+        }
     }
 }
